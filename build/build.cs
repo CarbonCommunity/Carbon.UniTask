3025081f7e9a1697e86d7c1288f5f1d1458b6857
raul <raulssorban@gmail.com>
2025-04-08 18:09:36 +0200

    Test

--- build/build.cs
+++ build/build.cs
@@ -7,5 +7,3 @@ DotNet.ExitOnError(true);
 DotNet.Run("restore", PathEnquotes(Home));
 DotNet.Run("clean", PathEnquotes(Home), "--configuration", target);
 DotNet.Run("build", PathEnquotes(Home), "--configuration", target, "--no-restore", $"/p:Version=\"{version}\"");
-
-Archive.Zip(Path(Home, "src", "UniTask.NetCore", "bin", target, "netstandard2.1"), Path(Home, "src", "UniTask.NetCore", "bin", target, "Carbon.UniTask.zip"));
