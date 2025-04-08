106d7fa3ac6468c13572013abb2923152c4de1ba
Oleg <69988794+DezLife@users.noreply.github.com>
2025-04-08 03:23:24 +0300

    init

---
+++ src/UniTask.NetCore/UniTaskInjector.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using UnityEngine;
+using UnityEngine.LowLevel;
+
+namespace Cysharp.Threading.Tasks;
+
+public class UniTaskInjector
+{
+	public static void Inject(SynchronizationContext syncContext, int threadId)
+	{
+		if (PlayerLoopHelper.IsInjectedUniTaskPlayerLoop()) return;
+
+		PlayerLoopHelper.unitySynchronizationContext = syncContext;
+		PlayerLoopHelper.mainThreadId = threadId;
+		try
+		{
+			PlayerLoopHelper.applicationDataPath = Application.dataPath;
+		}
+		catch { }
+		
+		if (PlayerLoopHelper.runners != null) return;
+
+		PlayerLoopSystem playerLoop = PlayerLoop.GetCurrentPlayerLoop();
+		PlayerLoopHelper.Initialize(ref playerLoop);
+	}
+}
