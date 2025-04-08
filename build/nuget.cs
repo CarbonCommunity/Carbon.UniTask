10b26b500c6e3483da01b2a3528af31b18cf9078
raul <raulssorban@gmail.com>
2025-04-08 17:48:01 +0200

    Nuget fix

--- build/nuget.cs
+++ build/nuget.cs
@@ -1,9 +1,13 @@
 var key = GetArg(1);
-var version = GetArg(2);
+var target = GetArg(2, "Debug");
+var version = GetVariable("VERSION");
 
 Warn($"Key: {key.Length}");
 Warn($"Version: {version}");
+Warn($"Target: {target}");
+
+SetHome(Path(Home, ".."));
 
 DotNet.SetQuiet(true);
-DotNet.Run("nuget", "push", PathEnquotes(Home, "Carbon.Core", ".nugets", $"Carbon.UniTask.{version}.nupkg"),
+DotNet.Run("nuget", "push", PathEnquotes(Home, "src", "UniTask.NetCore", "bin", target, $"Carbon.UniTask.{version}.nupkg"),
 	"--api-key", key, "--source", "https://api.nuget.org/v3/index.json");
