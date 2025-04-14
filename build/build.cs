d772e89abb464268816dfbe225f4ffe644ee79ef
raul <raulssorban@gmail.com>
2025-04-14 22:10:18 +0200

    Deleted Carbon submodule & standalone build github action/runner

--- build/build.cs
+++
@@ -1,9 +0,0 @@
-var target = GetArg(1, "Debug");
-var version = GetVariable("VERSION") ?? "1.0.0";
-
-SetHome(Path(Home, ".."));
-
-DotNet.ExitOnError(true);
-DotNet.Run("restore", PathEnquotes(Home));
-DotNet.Run("clean", PathEnquotes(Home), "--configuration", target);
-DotNet.Run("build", PathEnquotes(Home), "--configuration", target, "--no-restore", $"/p:Version=\"{version}\"");
